5662e1e16d1cc02ba483e0ba2c737756cf233443
Antun Matanović <antun.matanovic@gmail.com>
2019-01-17 23:06:11 +0100

    second lecture

---
+++ 02_lecture/3_sat/09_For.php
@@ -0,0 +1,45 @@
+<?php
+
+
+for($i=0;$i<10;$i++){
+    echo $i, "<br/>";
+}
+
+
+
+echo "Osijek<br/>";
+
+for($i=0;$i<100;$i++){
+    if($i===5){
+        break; //shortcuircuiting
+    }
+    if($i!=3){
+       continue;  //shortcuircuiting
+    }
+
+    echo $i. " Osijek<br/>";
+   
+}
+
+echo "Osijek<br/>";
+
+//nested loops
+
+for($i=0;$i<5;$i++){
+    for($j=0;$j<5;$j++){
+        echo $i+$j, " ";
+    }   
+    echo "<br />";
+}
+
+//break outer loop http://php.net/manual/en/control-structures.break.php
+outer:
+for($i=0;$i<5;$i++){
+    for($j=0;$j<5;$j++){
+       if($j===2){
+           break 2;
+       }
+       echo $i+$j, " ";
+    }   
+    echo "<br />";
+}
