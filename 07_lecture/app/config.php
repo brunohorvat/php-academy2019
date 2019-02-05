9a37d3bff9b1193461718f2690e8b4299c8023de
Tomislav Jakopec <tjakopec@gmail.com>
2019-02-05 16:50:35 +0100

    07. lecture

---
+++ 07_lecture/app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'url' => 'http://localhost/php-academy2019profesori/07_lecture/',
+    'mode'  => 'development',
+    "db"=>[
+        "host"=>"localhost",
+        "name"=>"social_network",
+        "user"=>"root",
+        "password"=>""
+    ]
+];
